f605df9184b4bc993296f802c8645506b9f3e3e1
Collin <cjsidler@gmail.com>
2021-03-27 18:59:25 -0700

    reworked..pretty much everything

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
-// After you do 9 + 3 and hit enter you get 12
-// If you then hit + 10 you will get 15 because secondNum
-// is not getting updated
+// What if you do 9 + 3 + 4?
 
-// Also, what if you do 9 + 3 + 4? It doesn't update
-// the calculation after hitting the second operator
+// Potential features to add: Limit button clicking to left clicks only
 
 let calcWrapper = document.getElementById('calculator-wrapper');
 let calcEle = document.getElementById('calculation');
 
-let firstNum = 0;
-let secondNum = 0;
+let calcArr = [];
+const operators = ['÷', '×', '−', '+'];
+let tempNum = 'none';
+let tempOperator = 'none';
 
-let operation = 'none'; // none, divide, multiply, subtract, or add
-let calculation = 'none';
-
+// Updates the number on the display when a user presses a number or decimal button
 function updateNum(event) {
     let buttonPressed = event.target;
+    let displayedNum = calcEle.innerText;
 
     // Prevents the user from entering a number greater than 10 digits
     // inclusive of a decimal point
-    if (calcEle.innerText.length + 1 > 10) {
+    if (displayedNum.length + 1 > 10 || displayedNum === 'error' || displayedNum === 'NaN') {
         return;
     }
 
-    if (buttonPressed.classList.contains('main-button') && (calcEle.innerText === '0' || calcEle.innerText === '')) {
+    // if length of calcArr is 1 or 3 and user presses a number, assume they want to start a new calculation
+    if (calcArr.length === 3 || calcArr.length === 1) {
+        // clear the array
+        calcArr = [];
+        // update the display with the number pressed or '0.' if they press decimal
         if (buttonPressed.innerText === '.') {
             calcEle.innerText = '0.';
+            return;
         } else {
             calcEle.innerText = buttonPressed.innerText;
+            return;
         }
-    } else if (buttonPressed.classList.contains('main-button') && calcEle.innerText !== '0' && calcEle.innerText !== 'error' && calcEle.innerText !== 'NaN') {
-        // If there isn't already a decimal, then append number to calcEle
-        if (calcEle.innerText.indexOf('.') === -1) {
-            calcEle.innerText = calcEle.innerText + buttonPressed.innerText;
-        } else if (calcEle.innerText.indexOf('.') >= 0 && buttonPressed.innerText !== '.') {
-            calcEle.innerText = calcEle.innerText + buttonPressed.innerText;
+    }
+
+    if (displayedNum === '0' || displayedNum === '') {
+        if (buttonPressed.innerText === '.') {
+            // if display shows 0 or is blank and the user presses '.', update display to '0.'
+            calcEle.innerText = '0.';
+        } else {
+            // if display shows 0 or is blank and user presses a number, update display to number pressed
+            calcEle.innerText = buttonPressed.innerText;
+        }
+    } else if (displayedNum !== '0' && displayedNum !== 'error' && displayedNum !== 'NaN') {
+        // if display has more than a 0 (for example 0. or any other number)
+        if (displayedNum.indexOf('.') === -1) {
+            // if there is no decimal somewhere, add the number/decimal pressed to the end
+            calcEle.innerText = displayedNum + buttonPressed.innerText;
+        } else if (displayedNum.indexOf('.') >= 0 && buttonPressed.innerText !== '.') {
+            // if there is a decimal and they pressed a number, add the number pressed to the end
+            calcEle.innerText = displayedNum + buttonPressed.innerText;
         }
     }
 }
@@ -48,7 +64,7 @@ function changeButtonColor(event) {
             buttonPressed.style.backgroundColor = 'var(--color-functions-pressed)';
         } else if (buttonPressed.classList.contains('main-button')) {
             buttonPressed.style.backgroundColor = 'var(--color-numbers-pressed)';
-        } else if (buttonPressed.classList.contains('operator')) {
+        } else if (buttonPressed.classList.contains('operator') || buttonPressed.classList.contains('equals')) {
             buttonPressed.style.backgroundColor = 'var(--color-operators-pressed)';
         }
     }
@@ -58,16 +74,16 @@ function changeButtonColor(event) {
             buttonPressed.style.backgroundColor = 'var(--color-functions)';
         } else if (buttonPressed.classList.contains('main-button')) {
             buttonPressed.style.backgroundColor = 'var(--color-numbers)';
-        } else if (buttonPressed.classList.contains('operator')) {
+        } else if (buttonPressed.classList.contains('operator') || buttonPressed.classList.contains('equals')) {
             buttonPressed.style.backgroundColor = 'var(--color-operators)';
         }
     }
 
 }
 
-// Checks to see if innerText of the element is out of range
+// Checks to see if a float is out of range
 function outOfRange(num) {
-    if (num > 999999999 || num < -999999999) {
+    if (num > 9999999999 || num < -9999999999) {
         return true;
     } else {
         return false;
@@ -76,109 +92,129 @@ function outOfRange(num) {
 
 // Function button clicked (AC to clear, DEL to delete one digit if no operator button was pressed)
 function funcButton(event) {
-    let buttonPressed = event.target;
+    let buttonPressed = event.target.innerText;
 
-    if (buttonPressed.classList.contains('function')) {
-        if (buttonPressed.innerText === 'AC') {
+    if (buttonPressed === 'AC') {
+        calcEle.innerText = '0';
+        calcArr = [];
+        tempNum = 'none';
+        tempOperator = 'none';
+    } else if (buttonPressed === 'DEL' && calcEle.innerText !== 'error' && calcEle.innerText !== 'NaN') {
+        // if calcEle.innerText is...
+            // any single digit or 0., make it 0
+        if (calcEle.innerText === '0.' || calcEle.innerText.length === 1) {
             calcEle.innerText = '0';
-            firstNum = 0;
-            secondNum = 0;
-            calculation = 'none';
-            operation = 'none';
-        } else if (buttonPressed.innerText === 'DEL' && calcEle.innerText !== 'error' && calcEle.innerText !== 'NaN') {
-            // if calcEle.innerText is...
-                // any single digit or 0., make it 0
-            if (calcEle.innerText === '0.' || calcEle.innerText.length === 1) {
-                calcEle.innerText = '0';
-            } else {
-                // #.#, remove last char
-                // multiple nums, remove last char
-                calcEle.innerText = calcEle.innerText.slice(0, -1);
-            }
+        } else {
+            // #.#, remove last char
+            // multiple nums, remove last char
+            calcEle.innerText = calcEle.innerText.slice(0, -1);
         }
     }
 }
 
-function updateOperation(event) {
-    let buttonPressed = event.target;
+// Calls the appropriate arithmetic function
+function calculate(num1, num2, operation) {
+    switch(operation) {
+        case '÷':
+            return parseFloat(divide(num1, num2).toFixed(4));
+        case '×':
+            return parseFloat(multiply(num1, num2).toFixed(4));
+        case '−':
+            return parseFloat(subtract(num1, num2).toFixed(4));
+        case '+':
+            return parseFloat(add(num1, num2).toFixed(4));
+    }
+}
 
+// Handles scenarios where the equals button is pressed
+function equals(event) {
+
+    // if display is showing an error, do nothing
     if (calcEle.innerText === 'error' || calcEle.innerText === 'NaN') {
         return;
     }
 
-    if (buttonPressed.classList.contains('operator') && event.target.innerText === '=') {
-        // If they clicked '=', perform the operation stored in 'operation' between firstNum and secondNum
+    // if user presses = and..
+    if (calcArr.length === 0) {
+        // calcArr is empty, push the number on display into array:
+        calcArr.push(parseFloat(calcEle.innerText));
+    } else if (calcArr.length === 1) {
+        // calcArr has one item in it, replace it with number on display
+        calcArr[0] = parseFloat(calcEle.innerText);
+    } else if (calcArr.length === 2) {
+        // if length is 2
+            // if display is blank, push 0 to calcArr
+            if (calcEle.innerText === '') {
+                calcArr.push(0);
+            } else {
+                calcArr.push(parseFloat(calcEle.innerText));
+            }
+            // calculate calcArr[0] and calcArr[2] and display on screen
+            calcEle.innerText = calculate(calcArr[0], calcArr[2], calcArr[1]);
+    } else if (calcArr.length === 3) {
+        calcArr = [];
+        calcArr.push(parseFloat(calcEle.innerText));
+    }
 
-        secondNum = parseFloat(calcEle.innerText);
-        // if (calculation === 'none') {
-        //     secondNum = parseFloat(calcEle.innerText);
-        // }
+    // Verify displayed number isn't out of range or too long
+    if (outOfRange(parseFloat(calcEle.innerText)) || calcEle.innerText.length > 10) {
+        calcEle.innerText = 'error';
+    }
+}
 
-        if (operation === 'none') {
-            return;
-        }
+// Handles scenarios where an operator button is pressed
+function updateOperation(event) {
+    let operatorUsed = event.target.innerText;
+    let currentDisplay = calcEle.innerText;
 
-        switch(operation) {
-            case 'divide':
-                calculation = divide(firstNum, secondNum).toFixed(4);
-                break;
-            case 'multiply':
-                calculation = multiply(firstNum, secondNum).toFixed(4);
-                break;
-            case 'subtract':
-                calculation = subtract(firstNum, secondNum).toFixed(4);
-                break;
-            case 'add':
-                calculation = add(firstNum, secondNum).toFixed(4);
-                break;
-        }
-        // Then show calculated result in display div (check if overflow error and make sure to limit result to 4 decimal places)
-        if (outOfRange(parseFloat(calculation))) {
-            calcEle.innerText = 'error';
-        } else if (calcEle.innerText.length + 1 > 10) {
-            calcEle.innerText = 'error';
+    if (currentDisplay === 'error' || currentDisplay === 'NaN') {
+        return;
+    }
+
+    if (calcArr.length === 0) {
+        // if they hit an operator and calcArr is empty
+            // add number on display to array
+            calcArr.push(parseFloat(calcEle.innerText));
+            // add operator to array
+            calcArr.push(operatorUsed);
+            // change display to blank to make it ready for a new number
+            calcEle.innerText = '';
+    } else if (operators.indexOf(calcArr[calcArr.length - 1]) >= 0) {
+        // if they hit an operator and the last item in calcArray is an operator and the display is blank
+            // override the operator with the one pressed
+        if (calcEle.innerText === '') {
+            calcArr[calcArr.length - 1] = operatorUsed;
         } else {
-            calcEle.innerText = parseFloat(calculation);
-            firstNum = parseFloat(calculation);
+            // if display is not blank perform calculation for calcArr[0] and displayed num with operator calcArr[1]
+            calcEle.innerText = calculate(calcArr[0], parseFloat(currentDisplay), calcArr[1]);
+            // clear array
+            calcArr = [];
+            // push calculation to array
+            calcArr.push(parseFloat(calcEle.innerText));
+            calcArr.push(operatorUsed);
+            calcEle.innerText = '';
         }
-        
-        // Move the calculated result into firstNum and leave operation and secondNum in case they want to hit '=' again and again
-
-    } else if (buttonPressed.classList.contains('operator')) {
-        // If they clicked a different operator (multiply, add, etc.), change 'operation' to the appropriate operator and store the display number into firstNum (up to a maximum of 4 decimal places)
-
-        switch(buttonPressed.innerText) {
-            case '÷':
-                operation = 'divide';
-                // if innerText === '' then don't update firstNum
-                updateFirst(calcEle.innerText);
-                calcEle.innerText = '';
-                break;
-            case '×':
-                operation = 'multiply';
-                updateFirst(calcEle.innerText);
-                calcEle.innerText = '';
-                break;
-            case '−':
-                operation = 'subtract';
-                updateFirst(calcEle.innerText);
-                calcEle.innerText = '';
-                break;
-            case '+':
-                operation = 'add';
-                updateFirst(calcEle.innerText);
-                calcEle.innerText = '';
-                break;
-        }
+    } else if (calcArr.length === 1) {
+        // if they hit an operator and calcArray has something in it and the length is 1
+            // add the operator to the array
+            calcArr.push(operatorUsed);
+            calcEle.innerText = '';
+    } else if (calcArr.length === 3) {
+        // if they hit an operator and calcArray has 3 items in it
+            // calculate operation on numbers in array
+            tempNum = calculate(calcArr[0], calcArr[2], calcArr[1]);
+            // clear array, push tempNum and operatorUsed to calcArr
+            calcArr = [];
+            calcArr.push(tempNum);
+            calcArr.push(operatorUsed);
+            // change display to blank to make it ready for a new number
+            calcEle.innerText = '';
+            // clear tempNum
+            tempNum = 'none';
     }
 }
 
-function updateFirst(string) {
-    if (string !== '') {
-        firstNum = parseFloat(calcEle.innerText);
-    }
-}
-
+// Arithmetic functions
 function divide(num1, num2) {
     return num1 / num2;
 }
@@ -195,18 +231,24 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
-// Event handlers
-let buttonHandler = (event) => { 
-    changeButtonColor(event);
-    updateNum(event);
-    funcButton(event);
-    updateOperation(event);
- }
-
-
 // Event listeners
-calcWrapper.addEventListener('mousedown', buttonHandler);
+calcWrapper.addEventListener('mousedown', changeButtonColor);
 calcWrapper.addEventListener('mouseup', changeButtonColor);
+document.querySelector('.equals').addEventListener('mousedown', changeButtonColor);
+document.querySelector('.equals').addEventListener('mousedown', equals);
+
+document.querySelectorAll('.main-button').forEach(element => {
+    element.addEventListener('mousedown', updateNum);
+});
+
+document.querySelectorAll('.function').forEach(element => {
+    element.addEventListener('mousedown', funcButton);
+});
+
+document.querySelectorAll('.operator').forEach(element => {
+    element.addEventListener('mousedown', updateOperation);
+});
+
 document.querySelectorAll('.button').forEach(element => {
     element.addEventListener('mouseleave', changeButtonColor);
 });
